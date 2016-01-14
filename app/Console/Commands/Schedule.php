1cda81e8790e453212e02cd4dbbd67f6f1962350
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-01-14 12:27:52 +0000

    Add command for schedule

---
+++ app/Console/Commands/Schedule.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\ScheduleScraper;
+use Illuminate\Console\Command;
+
+class Schedule extends Command
+{
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'alerj:schedule';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Scrape the schedule from alerj.gov.br.';
+
+    /**
+     * Create a new command instance.
+     *
+     */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+    /**
+     * Execute the console command.
+     *
+     * @param ScheduleScraper $scraper
+     * @return mixed
+     */
+	public function fire(ScheduleScraper $scraper)
+	{
+		$this->info('Scraping schedule...');
+		$scraper->scrapeToDatabase();
+	}
+
+	/**
+	 * Get the console command arguments.
+	 *
+	 * @return array
+	 */
+	protected function getArguments()
+	{
+		return [];
+	}
+
+	/**
+	 * Get the console command options.
+	 *
+	 * @return array
+	 */
+	protected function getOptions()
+	{
+		return [];
+	}
+}
