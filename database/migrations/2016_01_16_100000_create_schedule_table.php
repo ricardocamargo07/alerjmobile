c16f7c2927d3082a9c7332e0c04be9807a8b7615
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-01-14 12:27:35 +0000

    Add migration for schedule

---
+++ database/migrations/2016_01_16_100000_create_schedule_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateScheduleTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('schedule', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('alerj_id')->index();
+            $table->string('title');
+            $table->text('document');
+            $table->timestamp('date');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('schedule');
+	}
+}
