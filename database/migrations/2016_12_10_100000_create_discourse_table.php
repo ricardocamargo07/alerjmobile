bacfab8edb427469f698ce80974f1d970df83ddc
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-12-10 15:21:03 +0000

    Add discourses table

---
+++ database/migrations/2016_12_10_100000_create_discourse_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDiscourseTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('discourses', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('alerj_id')->index();
+            $table->string('title');
+            $table->integer('congressman_id')->unsigned();
+            $table->string('session_type')->nullable(); // sessao ordinaria
+            $table->string('expedient_type')->nullable(); // expediente final
+            $table->string('item_type')->nullable(); // discurso
+            $table->timestamp('datetime');
+            $table->text('document');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('discourses');
+	}
+}
