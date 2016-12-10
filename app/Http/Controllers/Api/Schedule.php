2f02525dfa194b7c63a0ce0f0fba3cc42b3e7003
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-12-10 20:39:51 +0000

    Add discourses to api

--- app/Http/Controllers/Api/Schedule.php
+++ app/Http/Controllers/Api/Schedule.php
@@ -3,28 +3,44 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Services\Scrapers\Scraper as ScheduleScraper;
+use App\Services\Scrapers\Schedule as ScheduleScraper;
+use App\Services\Scrapers\Discourse as DiscourseScraper;
 
 class Schedule extends Controller
 {
-	private $schedule;
+	private $scheduleScraper;
+    /**
+     * @var DiscourseScraper
+     */
+    private $discourseScraper;
 
-	public function __construct(ScheduleScraper $schedule)
+    public function __construct(ScheduleScraper $schedule, DiscourseScraper $discourseScraper)
 	{
-		$this->schedule = $schedule;
-	}
+		$this->scheduleScraper = $schedule;
+
+        $this->discourseScraper = $discourseScraper;
+    }
 
 	public function all()
 	{
-		return $this->response($this->schedule->all());
+        $schedule = collect($this->scheduleScraper->all()->toArray());
+
+        $discourse = collect($this->discourseScraper->all()->toArray());
+
+        $data = $schedule->merge($discourse);
+
+		return $this->response($data->sortByDesc('datetime'));
 	}
 
 	public function item($item)
 	{
+	    if (! $document = $this->scheduleScraper->getDocument($item))
+        {
+            $document = $this->discourseScraper->getDocument($item);
+        }
+
 		return $this->response(
-			$this->removeUneededLinks(
-				$this->schedule->getDocument($item)
-			)
+			$this->removeUneededLinks($document)
 		);
 	}
 }
