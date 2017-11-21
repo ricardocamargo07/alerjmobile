a82473245ecdbcc3a537c9e0e832abe02c91eb1f
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-11-21 14:50:36 -0200

    Clean debugging code

--- app/Http/Controllers/Api/AdmUser.php
+++ app/Http/Controllers/Api/AdmUser.php
@@ -26,41 +26,20 @@ class AdmUser extends Controller
 
     public function permissions(AdmUserService $admUserService, Request $request)
     {
-        info('line 1');
         $key = $this->makeKey(($username = $request->get('username')).($system = $request->get('system')));
 
-        info('line 2');
-//        if ($cached = $this->getCached($key))
-//        {
-//            info('line 3');
-//            return $cached;
-//        }
-
-        info('line 4');
-        info("---- username: $username -- system: $system -- key: ".gettype($key));
-        info('---- type = $key == '.gettype($key));
-        info($key);
+        if ($cached = $this->getCached($key))
+        {
+            return $cached;
+        }
 
         $result = $admUserService->getPermissions($username, $system);
 
-        info('line 4.5');
-        info('---- type = $key == '.gettype($result));
-        info($result);
-
         $result = array_utf8_converter($result);
-
-        info('line 5');
-        info('---- type = $result '.gettype($result));
-        info($result);
 
         $this->cache($key, $result);
 
         $result = json_encode($result);
-
-        info('line 6');
-        info('---- type = $result '.gettype($result));
-        info('---- json error = '.json_last_error_msg());
-        info($result);
 
         return $result;
     }
